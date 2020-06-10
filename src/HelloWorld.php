3c2cc3698179a96c4c7c069385e50f9597698301
wang3kai <wang3kai@163.com>
2020-06-10 22:32:01 +0800

    sth

---
+++ src/HelloWorld.php
@@ -0,0 +1,14 @@
+<?php
+namespace Soeasy\Demo
+class HelloWorld
+{
+    public function __construct($name = 'Soeasy')
+    {
+        $this->name = $name;
+    }
+    public function hello()
+    {
+        return 'Hello '.$this->name.'!';
+    }
+}
+?>
